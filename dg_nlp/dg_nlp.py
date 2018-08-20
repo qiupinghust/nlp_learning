3714fbc8f0a51c3266e0b0d073dacf8b7366cb68
weiyashui <1220292113@qq.com>
2018-08-20 23:47:02 +0800

    达观nlpp

--- dg_nlp/dg_nlp.py
+++ dg_nlp/dg_nlp.py
@@ -44,21 +44,21 @@ test_df = pd.concat(df_test_data, ignore_index=True)
 vec = TfidfVectorizer(ngram_range=(1,2),min_df=3, max_df=0.9,use_idf=1,smooth_idf=1, sublinear_tf=1,max_features=100000)
 #vec = HashingVectorizer(ngram_range=(1, 2), n_features=2**16)
 
-trn_term_doc = vec.fit_transform(train_df[column])
-test_term_doc = vec.transform(test_df[column])
+vec_train = vec.fit_transform(train_df[column])
+vec_test = vec.transform(test_df[column])
 y=(train_df["class"]-1).astype(int)
 #这里可以调优
 #lin_clf = svm.LinearSVC(C=0.1) #78.01%
 #交叉验证
 lin_clf = LogisticRegression(C=10,max_iter=1000) #78.40%
-scores_count=cross_val_score(lin_clf,trn_term_doc,y,cv=10,scoring='accuracy')
+scores_count=cross_val_score(lin_clf,vec_train,y,cv=10,scoring='accuracy')
 print(scores_count.mean())
 
-lin_clf.fit(trn_term_doc,y)
+lin_clf.fit(vec_train,y)
 joblib.dump(lin_clf, "train_model.m")
 clf = joblib.load("train_model.m")
 
-preds = lin_clf.predict(test_term_doc)
+preds = lin_clf.predict(vec_test)
 
 test_df["class"] = [x+1 for x in preds]
 
